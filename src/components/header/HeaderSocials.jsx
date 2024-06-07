3a63d0dfbc8da7670d3e92692b1db63c95611813
aayush3416 <87783633+aayush3416@users.noreply.github.com>
2024-06-06 23:48:11 -0400

    update linkedIn

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
-import { BsLinkedin, BsGithub } from 'react-icons/bs'
-import {MdOutlineEmail} from 'react-icons/md'
+import React from 'react';
+import { FaLinkedin, FaGithub, FaEnvelope } from 'react-icons/fa';
+import './header.css';
+
 const HeaderSocials = () => {
   return (
     <div className="header__socials">
-      <a href="https://www.linkedin.com/in/aayush-soni-8282bb200/" target="_blank"><BsLinkedin/></a>
-      <a href="https://github.com/aayush3416" target="_blank"><BsGithub /></a>
-      <a href="mailto:aayush.soni@uwaterloo.ca" target="_blank"><MdOutlineEmail/></a>
+      <a href="https://www.linkedin.com/in/aayush-soni-8282bb200/" target="_blank" rel="noopener noreferrer">
+        <FaLinkedin />
+      </a>
+      <a href="https://github.com/aayush-soni" target="_blank" rel="noopener noreferrer">
+        <FaGithub />
+      </a>
+      <a href="mailto:aayush.soni@example.com">
+        <FaEnvelope />
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default HeaderSocials
+export default HeaderSocials;
